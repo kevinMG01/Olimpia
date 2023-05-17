443b1952718fbc4a56f6ff7f8d7c938fe0c323fa
kevinMG01 <montanokevin46@gmail.com>
2023-05-16 21:14:54 -0300

    nada
    d

--- Olimpia/Mundo/Menu/Menu.gd
+++ Olimpia/Mundo/Menu/Menu.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
 
+func _ready():
+	$Camera2D.global_position = get_tree().get_nodes_in_group("spawn_2")[0].global_position
+
+func _physics_process(delta):
+	pass
+	
 
 
 func _on_1_button_down():
+	$Camera2D.global_position = get_tree().get_nodes_in_group("spawn_1")[0].global_position
+	pass # Replace with function body.
+
+
+func _on_2_button_down():
+	$Camera2D.global_position = get_tree().get_nodes_in_group("spawn_2")[0].global_position
 	pass # Replace with function body.
